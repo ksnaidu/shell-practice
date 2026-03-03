f2c163ac0482e91d049388f872a74c178e05fb6a
ksnaidu <ksnaidudba@gmail.com>
2026-03-03 10:54:22 +0530

    special-vars

--- 08.special-vars.sh
+++ 08.special-vars.sh
@@ -3,3 +3,9 @@
 echo "All variables passed to the script: $@"
 
 echo "Number of variables: $#"
+
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
