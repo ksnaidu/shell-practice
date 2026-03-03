be6e192ed6df9a21e816f16b1d20c47adf3f308a
ksnaidu <ksnaidudba@gmail.com>
2026-03-03 10:39:49 +0530

    arrays

--- 07.arrays.sh
+++ 07.arrays.sh
@@ -3,7 +3,7 @@
 MOVIES=("Court" "HIT3" "PUSHPA2" "RRR")
 
 #echo "First Movie: ${MOVIES[0]}"
-echo "last Movie: ${MOVIES[3]}"
-#echo "First Movie: ${MOVIES[4]}"
+#echo "last Movie: ${MOVIES[3]}"
+echo "First Movie: ${MOVIES[4]}"
 
 #echo "All movies: ${MOVIES[@]}"
