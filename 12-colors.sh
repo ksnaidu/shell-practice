391087ab332f6bfc93bc30726bdc201ccfbacd93
ksnaidu <ksnaidudba@gmail.com>
2026-03-06 10:44:15 +0530

    colors.sh

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#echo -e "\e[31m Hello Colors"
+echo -e "\e[31m Hello Colors"
 
-echo -e “\e[31m Hello world" 
-#echo -e “\e[32m Hello world” 
-#echo -e “\e[33m Hello world” 
-#echo -e “\e[0m hello world” 
